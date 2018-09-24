068df710ffd1f867a31c613f0cc2a622679d16bc
landanhsu <landanhsu.aplusaccelinc@gmail.com>
2018-09-24 16:10:00 +0800

    [feature/initial]
    .

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -13,21 +13,21 @@ import { Table } from 'element-ui'
   })
 class ETable extends Vue {
   @Prop()
-  rows!: Array<Object>
+  _rows!: Array<Object>
   // Typescript, must declare property of Class before we use this.data.
   // You only can use `data` or `props` in one time.
   // `data` means the property in porperty of Typescript.
   created () {
-    console.log('rows', this.rows)
+    console.log('rows', this._rows)
   }
 
   // get Class.method , computed method in Vue via Typescript
   public get __rows (): Array<Object> {
-    return this.rows
+    return this._rows
   }
 
   public set __rows (rows: Array<Object>) {
-    this.rows = rows
+    this._rows = rows
   }
 }
 
