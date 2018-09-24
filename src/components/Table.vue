a9bf2330cbb2425e884feb7f365e774df53a2a9e
landanhsu <landanhsu.aplusaccelinc@gmail.com>
2018-09-25 02:59:02 +0800

    .
    .

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,18 @@
 <template>
-  <Table v-bind:data="__rows"></Table>
+  <el-table :data="data">
+    <el-table-column
+      prop="date"
+      label="日期">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+  </el-table>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
@@ -9,25 +22,36 @@ import { Table } from 'element-ui'
   components: {
   Table: Table
   },
-  props: ['rows']
+  props: ['rows'],
+  data: function () {
+  return {
+  __rows: []
+  }
+  }
   })
 class ETable extends Vue {
-  @Prop()
-  _rows!: Array<Object>
+  vdata:Array<any> = [{
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }, {
+      date: '2016-05-04',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1517 弄'
+    }]
+
   // Typescript, must declare property of Class before we use this.data.
   // You only can use `data` or `props` in one time.
   // `data` means the property in porperty of Typescript.
   created () {
-    console.log('rows', this._rows)
   }
-
   // get Class.method , computed method in Vue via Typescript
-  public get __rows (): Array<Object> {
-    return this._rows
+  public get data (): Array<Object> {
+    return this.vdata
   }
 
-  public set __rows (rows: Array<Object>) {
-    this._rows = rows
+  public set data (data: Array<Object>) {
+    this.vdata = data
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import App from './App.vue'
 import indexRouter from './routers/index'
 import store from './store'
 import './registerServiceWorker'
-
+import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
