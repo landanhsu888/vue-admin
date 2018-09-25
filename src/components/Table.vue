0711567c685cb14fc8890cd373dde49fc91f9cf1
landanhsu <landanhsu.aplusaccelinc@gmail.com>
2018-09-25 16:02:50 +0800

    [feature/table]
    .

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,57 +1,36 @@
 <template>
-  <el-table :data="data">
-    <el-table-column
-      prop="date"
-      label="日期">
+  <el-table :data="tdata">
+    <el-table-column prop="date" label="日期">
     </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名">
+    <el-table-column prop="name" label="姓名">
     </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址">
+    <el-table-column prop="address" label="地址">
     </el-table-column>
   </el-table>
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
-import { Table } from 'element-ui'
 
 @Component({
-  components: {
-  Table: Table
-  },
-  props: ['rows'],
-  data: function () {
-  return {
-  __rows: []
-  }
-  }
+  props: ['rows']
   })
 class ETable extends Vue {
-  vdata:Array<any> = [{
-      date: '2016-05-02',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
-    }, {
-      date: '2016-05-04',
-      name: '王小虎',
-      address: '上海市普陀区金沙江路 1517 弄'
-    }]
-
+  @Prop({
+  })
+  rows: Array<Object> = this.rows
   // Typescript, must declare property of Class before we use this.data.
   // You only can use `data` or `props` in one time.
   // `data` means the property in porperty of Typescript.
   created () {
+    console.log(this.rows)
   }
   // get Class.method , computed method in Vue via Typescript
-  public get data (): Array<Object> {
-    return this.vdata
+  public get tdata (): Array<Object> {
+    return this.rows
   }
 
-  public set data (data: Array<Object>) {
-    this.vdata = data
+  public set tdata (rows: Array<Object>) {
+    this.tdata = rows
   }
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="./../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
     <Table v-bind:rows="users"></Table>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './../components/HelloWorld.vue' // @ is an alias to /src
-import Button from './../components/Button.vue' // @ is an alias to /src
 import Table from './../components/Table.vue' // @ is an alias to /src
 
 @Component({
   components: {
-  HelloWorld,
-  Button,
   Table
   }
   })
