18d1112a8b8458fb5105e23081580d04949a50ed
landanhsu <landanhsu.aplusaccelinc@gmail.com>
2018-10-12 14:13:28 +0800

    [feature/mixins_resize]
    ..

---
+++ src/mixins/resize.ts
@@ -0,0 +1,36 @@
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+import { debounce } from './../utils'
+
+@Component
+class Resize extends Vue {
+  public chart: any;
+  public __resizeHandler: any;
+
+  mounted() {
+    this.__resizeHandler = debounce(() => {
+      if (this.chart) {
+        this.chart.resize()
+      }
+    }, 100, void 0)
+    window.addEventListener('resize', this.__resizeHandler)
+
+    const sidebarElm = document.getElementsByClassName('sidebar-container')[0]
+    sidebarElm.addEventListener('transitionend', this.sidebarResizeHandler)
+  }
+
+  beforeDestroy() {
+    window.removeEventListener('resize', this.__resizeHandler)
+
+    const sidebarElm = document.getElementsByClassName('sidebar-container')[0]
+    sidebarElm.removeEventListener('transitionend', this.sidebarResizeHandler)
+  }
+
+  sidebarResizeHandler(e: any) {
+    if (e.propertyName === 'width') {
+      this.__resizeHandler()
+    }
+  }
+
+}
+export default Resize
