a661b21826504b8ca73123d990f22c351031f660
landanhsu <landanhsu.aplusaccelinc@gmail.com>
2018-10-15 14:23:04 +0800

    [feature/page_redirect]
    ..

--- src/pages/Redirect.vue
+++ src/pages/Redirect.vue
@@ -1,26 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="./../assets/logo.png" v-on:click="onInsert">
-    {{ "s tom" | capitalize }}
-    <Table :data="members" :labels="labels"></Table>
-    <BackToTop></BackToTop>
-    <UploadExcel></UploadExcel>
-    <Hamburger v-bind:toggleClick="onInsert"></Hamburger>
-    <SizeSelect></SizeSelect>
-    ---
-    <Bug :class="'svg-icon'"></Bug>
-  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component({
+  name: 'Redirect',
   components: {
   }
   })
 class Redirect extends Vue {
-
+  beforeCreate() {
+    const { params, query } = this.$route
+    const { path } = params
+    this.$router.replace({ path: '/' + path, query })
+  }
 }
 
 export default Redirect
