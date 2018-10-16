593bf6eabff0740b0a8821e24105898c8dd1a42e
landanhsu <landanhsu.aplusaccelinc@gmail.com>
2018-10-16 15:04:50 +0800

    ..
    ..

--- src/helpers/Axios/index.ts
+++ src/helpers/Axios/index.ts
@@ -1,129 +1,150 @@
 import axios from 'axios'
 
 class AxiosHelper {
+
+  /**
+   * @param {string} url
+   * @param {object | Array<object>} params
+   * @param {boolean} isPolling
+  */
   get (url: string, params: object | Array<object>, isPolling: boolean = false) {
     isPolling = !!isPolling;
 
     if (params instanceof Array) {
-        let paramses = params;
-        let resJsons: any[] = [];
-        if (isPolling) {
-          let nextPromise = function(i) {
-              if (i >= paramses.length) {
-                  return Promise.resolve(resJsons);
-              }
-              let params = paramses[i];
-              // _reqInit.cache = 'no-cache';
-              // _reqInit.mode = 'cors';
-              // _reqInit.credentials = 'include';
+      let paramses = params;
+      let resJsons: any[] = [];
+      if (isPolling) {
+        let nextPromise = function(i) {
+          if (i >= paramses.length) {
+              return Promise.resolve(resJsons);
+          }
+          let params = paramses[i];
+          // _reqInit.cache = 'no-cache';
+          // _reqInit.mode = 'cors';
+          // _reqInit.credentials = 'include';
 
-              return axios.get(url, params).then(function(resJson) {
-                  resJsons.push(resJson);
-                  return nextPromise(i + 1);
-              });
-          };
-          return nextPromise(0);
-        } 
+          return axios.get(url, params).then(function(resJson) {
+              resJsons.push(resJson);
+              return nextPromise(i + 1);
+          });
+        };
+        return nextPromise(0);
+      } 
 
-        return Promise.all(paramses.map((params) => {
-          return axios.get(url, params)
-        }));
+      return Promise.all(paramses.map((params) => {
+        return axios.get(url, params)
+      }));
     }
 
     return axios.get(url, params)
   }
 
+  /**
+   * @param {string} url
+   * @param {object | Array<object>} params
+   * @param {boolean} isPolling
+  */
   post (url: string, params: object | Array<object>, isPolling: boolean = false): Promise<object> | Promise<object[]> {
     isPolling = !!isPolling;
 
     if (params instanceof Array) {
-        let paramses = params;
-        let resJsons: any[] = [];
-        if (isPolling) {
-          let nextPromise = function(i) {
-              if (i >= paramses.length) {
-                  return Promise.resolve(resJsons);
-              }
-              let params = paramses[i];
-              // _reqInit.cache = 'no-cache';
-              // _reqInit.mode = 'cors';
-              // _reqInit.credentials = 'include';
+      let paramses = params;
+      let resJsons: any[] = [];
+      if (isPolling) {
+        let nextPromise = function(i) {
+          if (i >= paramses.length) {
+              return Promise.resolve(resJsons);
+          }
+          let params = paramses[i];
+          // _reqInit.cache = 'no-cache';
+          // _reqInit.mode = 'cors';
+          // _reqInit.credentials = 'include';
 
-              return axios.post(url, params).then(function(resJson) {
-                  resJsons.push(resJson);
-                  return nextPromise(i + 1);
-              });
-          };
-          return nextPromise(0);
-        } 
+          return axios.post(url, params).then(function(resJson) {
+              resJsons.push(resJson);
+              return nextPromise(i + 1);
+          });
+        };
+        return nextPromise(0);
+      } 
 
-        return Promise.all(paramses.map((params) => {
-          return axios.post(url, params)
-        }));
+      return Promise.all(paramses.map((params) => {
+        return axios.post(url, params)
+      }));
     }
 
     return axios.post(url, params)
   }
 
+  /**
+   * @param {string} url
+   * @param {object | Array<object>} params
+   * @param {boolean} isPolling
+  */
   put (url: string, params: object | Array<object>, isPolling: boolean = false) {
     isPolling = !!isPolling;
 
     if (params instanceof Array) {
-        let paramses = params;
-        let resJsons: any[] = [];
-        if (isPolling) {
-          let nextPromise = function(i) {
-              if (i >= paramses.length) {
-                  return Promise.resolve(resJsons);
-              }
-              let params = paramses[i];
-              // _reqInit.cache = 'no-cache';
-              // _reqInit.mode = 'cors';
-              // _reqInit.credentials = 'include';
+      let paramses = params;
+      let resJsons: any[] = [];
+      if (isPolling) {
+        let nextPromise = function(i) {
+          if (i >= paramses.length) {
+              return Promise.resolve(resJsons);
+          }
+          let params = paramses[i];
+          // _reqInit.cache = 'no-cache';
+          // _reqInit.mode = 'cors';
+          // _reqInit.credentials = 'include';
 
-              return axios.put(url, params).then(function(resJson) {
-                  resJsons.push(resJson);
-                  return nextPromise(i + 1);
-              });
-          };
-          return nextPromise(0);
-        } 
+          return axios.put(url, params).then(function(resJson) {
+              resJsons.push(resJson);
+              return nextPromise(i + 1);
+          });
+        };
+        return nextPromise(0);
+      } 
 
-        return Promise.all(paramses.map((params) => {
-          return axios.put(url, params)
-        }));
+      return Promise.all(paramses.map((params) => {
+        return axios.put(url, params)
+      }));
     }
 
     return axios.put(url, params)
   }
-
+  
+  /**
+   * @param {string} url
+   * @param {object | Array<object>} params
+   * @param {boolean} isPolling
+  */
   delete (url: string, params: object | Array<object>, isPolling: boolean = false) {
     isPolling = !!isPolling;
 
     if (params instanceof Array) {
-        let paramses = params;
-        let resJsons: any[] = [];
-        if (isPolling) {
-          let nextPromise = function(i) {
-              if (i >= paramses.length) {
-                  return Promise.resolve(resJsons);
-              }
-              let params = paramses[i];
-              // _reqInit.cache = 'no-cache';
-              // _reqInit.mode = 'cors';
-              // _reqInit.credentials = 'include';
+      let paramses = params;
+      let resJsons: any[] = [];
+      if (isPolling) {
+        let nextPromise = function(i) {
+          if (i >= paramses.length) {
+              return Promise.resolve(resJsons);
+          }
+          let params = paramses[i];
+          // _reqInit.cache = 'no-cache';
+          // _reqInit.mode = 'cors';
+          // _reqInit.credentials = 'include';
 
-              return axios.delete(url, params).then(function(resJson) {
-                  resJsons.push(resJson);
-                  return nextPromise(i + 1);
-              });
-          };
-          return nextPromise(0);
-        } 
+          return axios.delete(url, params).then(function(resJson) {
+              resJsons.push(resJson);
+              return nextPromise(i + 1);
+          });
+        };
+        return nextPromise(0);
+      } 
 
-        return Promise.all(paramses.map((params) => {
-          return axios.delete(url, params)
-        }));
+      return Promise.all(paramses.map((params) => {
+        return axios.delete(url, params)
+      }));
     }
 
     return axios.delete(url, params)
