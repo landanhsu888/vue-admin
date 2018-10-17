2b7e8c027c501f0642c64b387be79b8cdafab2a0
landanhsu <landanhsu.aplusaccelinc@gmail.com>
2018-10-17 10:58:41 +0800

    ..
    ..

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-  // root: true,
-  // env: {
-  //   node: true
-  // },
-  // 'extends': [
-  //   'plugin:vue/essential',
-  //   '@vue/standard',
-  //   '@vue/typescript'
-  // ],
-  // rules: {
-  //   'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  //   'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  //   'no-trailing-spaces': ['error', { 'skipBlankLines': true }],
-  //   'no-useless-escape': ['off']
-  // },
-  // parserOptions: {
-  //   parser: 'typescript-eslint-parser'
-  // }
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-trailing-spaces': ['error', { 'skipBlankLines': true }],
+    'no-useless-escape': ['off']
+  },
+  parserOptions: {
+    parser: 'typescript-eslint-parser'
+  }
 }
